0a06f4483d55259f787c804a8774a2b99d527b05
Ayyub Abdurrahman <2200016092@webmail.uad.ac.id>
2025-08-04 17:29:01 +0700

    Fix CSS Loading Issues

--- resources/js/services/api.js
+++ resources/js/services/api.js
@@ -1,7 +1,6 @@
 import axios from "axios";
-const API_URL = "http://localhost:8000/api";
+const API_URL = "/api";
 
 export const login = (data) => axios.post(`${API_URL}/login`, data);
 export const getProjects = () => axios.get(`${API_URL}/projects`);
 export const getUiUx = (project_id) => axios.get(`${API_URL}/uiux/${project_id}`);
-// Tambahkan endpoint lain sesuai kebutuhan 
